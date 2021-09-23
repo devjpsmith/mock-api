d214e3dae3b696bd323074e1c729b2a764815ca6
James <dev.jpsmith@gmail.com>
2021-09-23 13:47:24 -0600

    added orders endpoint that returns object array

--- index.js
+++ index.js
@@ -36,3 +36,70 @@ mockApiResponse.mock('/api/statusCode/', 'GET', [
         response: { statusCode: 500 }
     },
 ]);
+
+mockApiResponse.mock('/orders', 'GET', [
+    {
+        notes: 'this endpoint returns an array of orders. no parameters are required',
+        notes2: 'also, I can add any property I want here, it has no effect on the response',
+        name: 'orders get',
+        parameters: [],
+        response: {
+            statusCode: 200,
+            body: [
+                {
+                    'OrderReferenceID': '{{OrderReferenceID}}',
+                    'PickupDate': '{{pickup_date}}',
+                    'ServiceLevel': 'Threshold',
+                    'DropOffLocation': {
+                        'Address1': '5678 Destination Street',
+                        'Address2': 'Unit 101',
+                        'ProvinceState': 'MT',
+                        'City': 'Action',
+                        'ZipCode': '59002',
+                        'Country': 'US',
+                        'LocationType': 'Commercial'
+                    },
+                    'PickupLocation': {
+                        'Address1': '1234 Source Street',
+                        'Address2': 'Third Floor',
+                        'ProvinceState': 'WA',
+                        'City': 'Seattle',
+                        'ZipCode': '98101',
+                        'Country': 'US',
+                        'LocationType': 'Commercial'
+                    },
+                    'TotalDeclaredValue': {
+                        'Value': 500,
+                        'Unit': 'USD'
+                    },
+                    'Accessorials': [
+                    ],
+                    'Boxes': [
+                        {
+                            'Description': 'Pack of 4',
+                            'Sku': 'SKU12345',
+                            'UPCCode': '01234567890',
+                            'FreightClass': '50',
+                            'Quantity': 1,
+                            'DeclaredValue': {
+                                'Value': 500,
+                                'Unit': 'USD'
+                            },
+                            'Category': 'CasedGoodsFurniture',
+                            'Dimension': {
+                                'Unit': 'Inch',
+                                'Length': 10,
+                                'Width': 10,
+                                'Height': 10
+                            },
+                            'Weight': {
+                                'Value': 45,
+                                'Unit': 'LB'
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+]);
